87bf100486ce93c4dc7c12688b023e488a775104
aloha-source <alohaukulele0405@gmail.com>
2025-09-04 01:00:50 +0900

    Create 220_checkup_idx_visits.sql

---
+++ patches/220_checkup_idx_visits.sql
@@ -0,0 +1,11 @@
+/* 220_checkup_idx_visits.sql */
+-- @phase: idx
+-- @provides: index:uq_uuid, index:idx_visits_farm, index:idx_visits_farm_started, index:idx_visits_started, index:idx_visits_list, index:idx_visits_clinic
+-- @requires: table:visits
+
+CREATE UNIQUE INDEX uq_uuid           ON visits(uuid);
+CREATE INDEX idx_visits_farm         ON visits(farm_uuid);
+CREATE INDEX idx_visits_farm_started ON visits(farm_uuid, visit_started_at);
+CREATE INDEX idx_visits_started      ON visits(visit_started_at);
+CREATE INDEX idx_visits_list         ON visits(deleted_at, updated_at, id);
+CREATE INDEX idx_visits_clinic       ON visits(clinic_uuid, visit_started_at);
