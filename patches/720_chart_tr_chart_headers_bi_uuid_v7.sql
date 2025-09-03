5451ab158538f1fb268499069210116af5860e8f
aloha-source <alohaukulele0405@gmail.com>
2025-09-04 02:34:42 +0900

    Create 720_chart_tr_chart_headers_bi_uuid_v7.sql

---
+++ patches/720_chart_tr_chart_headers_bi_uuid_v7.sql
@@ -0,0 +1,17 @@
+/* 720_chart_tr_chart_headers_bi_uuid_v7.sql */
+-- @phase: trigger
+-- @provides: trigger:tr_chart_headers_bi_uuid_v7
+-- @requires: table:chart_headers, function:uuid_v7_bin
+
+DROP TRIGGER IF EXISTS tr_chart_headers_bi_uuid_v7;
+
+DELIMITER $$
+CREATE TRIGGER tr_chart_headers_bi_uuid_v7
+BEFORE INSERT ON chart_headers
+FOR EACH ROW
+BEGIN
+  IF NEW.uuid IS NULL OR LENGTH(NEW.uuid) = 0 THEN
+    SET NEW.uuid = uuid_v7_bin();
+  END IF;
+END $$
+DELIMITER ;
